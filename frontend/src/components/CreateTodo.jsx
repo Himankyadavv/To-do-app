368bec27088ad4c1008f35e71426c760fe8592fe
Himankyadavv <himanky218@gmail.com>
2024-07-26 21:48:26 +0530

    added front end

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,10 @@
+ export function CreateTodo(){
+    return <div>
+        <input type="text" placeholder="title" id="" />
+        <input type="text" placeholder="description" id="" /><br />
+
+        <button>Add task</button>
+    </div>
+ }
+
+ 
