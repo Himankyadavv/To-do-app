3456ae6d1d2fd16e4a2dbf4bafca1d2a59dac23e
Himankyadavv <himanky218@gmail.com>
2024-07-26 19:06:37 +0530

    changes have been made in index and mongo files

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,59 @@
 const express = require('express')
 const app = express();
 const port = 3000;
+import { dataSchema } from './mongo';
 import { createTodo } from './types';
 import { updateTodo } from './types';
 app.use(express.json())
 
-app.post('/todo',(req,res)=>{
+app.post('/todo', async (req,res)=>{
     const input = req.body;
     const parseinput = createTodo.safeParse(input);
     if(!parseinput.success){
         res.status(411).json({
             msg: "wrong input !"
         })
+        return ;
     }
+
+    await dataSchema.create({
+        title: input.title,
+        description: input.description,
+        completed: false
+    })
+
+    res.json({
+        msg: "To do created"
+    })
 })
 
-app.get('todos',(req,res)=>{
-
+app.get('todos',async (req,res)=>{
+    const todos = await dataSchema.find({})
+    res.json({
+        todos
+    })
 })
 
-app.put('completed',(req,res)=>{
+app.put('completed',async (req,res)=>{
     const udpateInput = req.body;
     const parseUpdateInput = updateTodo.safeParse(udpateInput);
     if(!parseUpdateInput.success){
         req.status(411).json({
             msg: "you send the wrong input "
         })
+        return;
     }
+
+    await dataSchema.udpate({
+        _id: req.body.id
+    }, {
+        completed: true
+    })
+
+    res.json({
+        msg: "to do marked as completed"
+    })
+
 })
 
 app.listen(port, ()=>{
--- backend/mongo.js
+++ backend/mongo.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
-const { string } = require('zod')
+const { string, boolean } = require('zod')
 
 mongoose.connect("mongodb+srv://Himank:Himank123@cluster-0.tbaskcb.mongodb.net/todo")
 
 const dataSchema = mongoose.model("todolist",{
     title: string,
-    description: string 
+    description: string,
+    completed: boolean
 })
+
+module.exports = {
+    dataSchema: dataSchema
+}
